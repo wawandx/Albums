ee4f8aacd3d600b6ba5d0f2f47cae4775d480a64
Rachmad Kurniawan <wawandx@gmail.com>
2019-04-16 09:29:05 +0800

    first Agesa

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import ReactNative from 'react-native';
+import Header from './src/components/header';
+
+const App = () => {
+  return (
+      <Header headerText = {'AGESA'} />
+  );
+}
+
+
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle, headerShadow } = styles;
+
+  return (
+    <View style = { viewStyle }>
+      <Text style = { textStyle } >{props.headerText}</Text>  
+    </View>    
+  );
+}
+
+const styles = {
+  viewStyle : {
+    backgroundColor : '#de4532',
+    justifyContent : 'center', //flex-start,center,flex-end
+    alignItems : 'center', //flex-start,center,flex-end
+    height : 70,
+    paddingTop : 20,
+    shadowColor : '#000',
+    shadowOffset : { width : 0, height: 2},
+    shadowOpacity : 0.2,
+    elevation : 5,
+    position : 'relative'
+  },
+  textStyle : {
+    fontSize : 20,
+    color : '#fff'
+  }
+}
+
+
+export default Header;
