90e36039e6cf219e822ccacf8e19ff6120f723cf
Rachmad Kurniawan <wawandx@gmail.com>
2019-04-16 09:39:29 +0800

    Agesa First

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const Header = (props) => {
-  const { textStyle, viewStyle, headerShadow } = styles;
+  const { textStyle, viewStyle } = styles;
 
   return (
     <View style = { viewStyle }>
